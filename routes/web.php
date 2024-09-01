98a1854df658db648631c7d1d2bf74beead3bf59
Luon Verak <luonverakcambo@gmail.com>
2024-09-01 14:43:19 +0700

    first_commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Views\BackendViewController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(["prefix" => "admin"], function () {
+
+    Route::get("/", [BackendViewController::class, "index"]);
+
+});
+
