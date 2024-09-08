e59e4d6a3da525da22e8a50b02f52a0b8e42632b
Luon Verak <luonverakcambo@gmail.com>
2024-09-08 14:30:03 +0700

    connect_db

--- public/asset/js/admin.js
+++ public/asset/js/admin.js
@@ -2,5 +2,9 @@ $(document).on("click", "button.open-category-modal", function () {
     $("div.category-modal").modal("show");
 }).on("click","button.accept-save-category",function () {  
     let name = $("div.category-modal").find("input#name").val();
-    console.log("name : "+name);
+    let description =$("div.category-modal").find("textarea#description").val();
+    let logo = $("div.category-modal").find("input#logo")[0].files[0];
+    console.log(name);
+    console.log(description);
+    console.log(logo);
 });
