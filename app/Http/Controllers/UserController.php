1ba6b228c6796495a4e9ff370e8feb2a449ae378
Verak Luon <luonverakcambo@gmail.com>
2024-10-13 16:43:53 +0700

    auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,121 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function register(){
-        
-    }
-    public function login(){
+    public function register(Request $request)
+    {
+        try {
+            if (!$request->has("first_name") || $request->first_name == null) {
+                return response()->json([
+                    "status" => "failed",
+                    "msg" => "First name is requried"
+                ]);
+            }
+            if (!$request->has("last_name") || $request->last_name == null) {
+                return response()->json([
+                    "status" => "failed",
+                    "msg" => "Last name is requried"
+                ]);
+            }
+            if (!$request->has("email") || $request->email == null) {
+                return response()->json([
+                    "status" => "failed",
+                    "msg" => "Email name is requried"
+                ]);
+            }
 
+            if (!$request->has("password") || $request->password == null) {
+                return response()->json([
+                    "status" => "failed",
+                    "msg" => "Password name is requried"
+                ]);
+            }
+
+            $firstName = $request->first_name;
+            $lastName = $request->last_name;
+            $email = $request->email;
+            $password = $request->password;
+
+            $user = new User();
+            $user->first_name = $firstName;
+            $user->last_name = $lastName;
+            $user->email = $email;
+            $user->password = $password;
+            $user->save();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+    public function login(Request $request)
+    {
+        try {
+            // if (!$request->has("email") || $request->email == null) {
+            //     return response()->json([
+            //         "status" => "failed",
+            //         "msg" => "Email name is requried"
+            //     ]);
+            // }
+
+            // if (!$request->has("password") || $request->password == null) {
+            //     return response()->json([
+            //         "status" => "failed",
+            //         "msg" => "Password name is requried"
+            //     ]);
+            // }
+
+            $email = $request->email;
+            $password = $request->password;
+
+            $credentials = $request->only(["email", "password"]);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json(
+                    [
+                        "status" => "failed",
+                        "msg" => "Something wrong login failed"
+                    ],
+                    Response::HTTP_UNAUTHORIZED
+                );
+            }
+
+            $user = User::where("email", $email)->first();
+            if (!$user) {
+                return response()->json(
+                    [
+                        "status" => "failed",
+                        "msg" => "Something wrong"
+                    ],
+                );
+            }
+            if (!Hash::check($password, $user->getAuthPassword())) {
+                return response()->json(
+                    [
+                        "status" => "failed",
+                        "msg" => "Something wrong"
+                    ],
+                );
+            } else {
+                if($user->tokens()->exists()){
+                    $user->tokens()->delete();
+                }
+                $token = $user->createToken("USER_TOKEN")->plainTextToken;
+                $cookie = cookie("jwt", $token, 1);
+                return response()->json([
+                    "status" => "success",
+                    "msg" => "Account login success",
+                    "user" => $user,
+                    "token" => $token
+                ])->withCookie($cookie);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post("/register",[UserController::class,"register"]);
+Route::post("/login",[UserController::class,"login"]);
+
 Route::group(["prefix" => "admin"], function () {
 
-    Route::post("/add-category", [CategoryController::class, "addCategory"]);
-    Route::post("/get-category", [CategoryController::class, "getCategory"]);
-    Route::post("/edit-category", [CategoryController::class, "editCategory"]);
+   
+    Route::middleware(["auth:sanctum"])->group(function () {
+    
+        Route::post("/add-category", [CategoryController::class, "addCategory"]);
+        Route::post("/get-category", [CategoryController::class, "getCategory"]);
+        Route::post("/edit-category", [CategoryController::class, "editCategory"]);
+    
+    });
 });
+
